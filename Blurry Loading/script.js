398219e3345bb340b96407d1961eb6d37b1e80c1
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-10-17 19:34:32 -0400

    complete scroll animation

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -1,12 +1,17 @@
 const loadText = document.querySelector(".percentage");
 const bg = document.querySelector(".bg");
 
-let time = 70;
+let load = 0;
 
 window.addEventListener("load", () => {
     setInterval(function Timer() {
-        time--;
-        bg.innerText = `${time}%`;
-        bg.style.filter = `blur(${time}px)`
+        load > 99 ? clearInterval() : load++;
+        loadText.innerText = `${load}%`;
+        loadText.style.opacity = scale(load, 0, 100, 1, 0);
+        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
     }, 30)
 });
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
