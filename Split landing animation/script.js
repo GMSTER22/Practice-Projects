3bf534f7d8b4759b0c617414a40509311d0efb63
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-10-24 22:42:08 -0400

    javascript of split landing animation complete

--- Split landing animation/script.js
+++ Split landing animation/script.js
@@ -1,1 +1,15 @@
+const ps = document.querySelector(".ps");
+const xbox = document.querySelector(".xbox");
 
+
+const stretchElement = (element) => {
+    element.addEventListener("mouseenter", () => {
+        element.classList.add("active");
+    })
+    element.addEventListener("mouseleave", () => {
+        element.classList.remove("active");
+    })
+}
+
+stretchElement(ps);
+stretchElement(xbox);
