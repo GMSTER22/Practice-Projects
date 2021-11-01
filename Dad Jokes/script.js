9de13b275c537c74b41b05564cfbda93c3298e42
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-11-01 16:47:34 -0400

    complete dad joke generator

---
+++ Dad Jokes/script.js
@@ -0,0 +1,31 @@
+const dadJokeUrl = "https://icanhazdadjoke.com/";
+const myHeader = { headers: { 
+                                Accept : "application/json"
+} };
+
+const joke = document.querySelector(".joke");
+const button = document.querySelector("button");
+
+const getJoke = () => {
+    fetch(dadJokeUrl, myHeader)
+                .then(res => {
+                    if (!res.ok) {
+                        console.log("Error", res.status)
+                    } else {
+                        return res.json()
+                    }                   
+                })
+                .then( data => {
+                    joke.innerHTML = data.joke;
+                    button.addEventListener("click", getJoke);
+                })
+                .catch(error => console.log("Error:", error))
+}
+
+getJoke()
+getJoke()
+getJoke()
+getJoke()
+
+
+
