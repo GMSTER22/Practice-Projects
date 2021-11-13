d48b20fa56bf0ac5f6e1dc63009ce95213ce903f
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-11-12 23:52:39 -0500

    complete the functionality of drink water'

--- Drink water challenge/script.js
+++ Drink water challenge/script.js
@@ -1,13 +1,31 @@
 //function select to fill any cup and prior cups to that one
-
 const smallCups = document.querySelectorAll(".small_cup");
-
-smallCups.addEventListener("click", () => {
-    fillCup()
-})
-
-console.log(smallCups)
+const bigCup = document.querySelector(".big_cup");
+const smallCupsFilled = document.getElementsByClassName("small_cup filled");
 
 const fillCup = (cup) => {
-    cup.classList.toggle("filled")
+    cup.classList.add("filled");
+}
+
+const emptyCup = (cup) => {
+    cup.classList.remove("filled");
+}
+
+for (let i = 0; i < smallCups.length; i++) {
+    smallCups[i].addEventListener("click", () => {
+        for (let j = 0; j <= i; j++) {
+            fillCup(smallCups[j]);
+
+            for (let k = smallCups.length -1; k > i; k-- ) {
+                emptyCup(smallCups[k]);
+            }            
+        }
+        let percentage = (smallCupsFilled.length/smallCups.length) * 100;
+
+        bigCup.style.backgroundImage = `linear-gradient(to top, #84bcf0 0, #84bcf0 ${percentage}%, transparent ${percentage}%)`;
+
+        bigCup.children[1].textContent = `${percentage}%`;
+        console.log(bigCup.children[1].textContent = `${percentage}%`)
+        
+    })
 }
