29f99bfc0c702188f1716f5b8b0074fce18e7f9b
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-11-18 15:24:56 -0500

    previous, next, and color mode buttons functionals

--- Movie App/script.js
+++ Movie App/script.js
@@ -8,28 +8,24 @@ const container = document.getElementById("main");
 const form = document.querySelector("#form");
 const search = document.querySelector("#search");
 
+const btns = document.querySelector(".btns");
 const prevBtn = document.querySelector("#previous"); 
 const nextBtn = document.querySelector("#next"); 
 
+const colorSwitch = document.querySelector("#switch");
 
-function getFollowingBtn () {
-
-}
-
-
-prevBtn.addEventListener("click", () => {
-    page > 1 ? page-- : page;
-    return page;
-})
-
+let page = 1;
+const maxPage = 500;
 
 //Will display the movies the client is searching for
 
 form.addEventListener("submit", function(e) {
     e.preventDefault()
+
     const searchInput = search.value;
 
     if (searchInput && searchInput !== "") {
+        document.querySelector("body").style.padding = "0 0 0 0";
         container.innerHTML = ""
         
         const getSpecificMovie = getMovies(SEARCH_URL + searchInput);
@@ -58,6 +54,8 @@ form.addEventListener("submit", function(e) {
 
                 container.appendChild(movieEl)                
             })
+
+            btns.style.display = "none";
         })
 
         search.value = ""
@@ -68,14 +66,7 @@ form.addEventListener("submit", function(e) {
 
 })
 
-let page = 1;
-
-// let getPopularMovies = getMovies(API_URL);
-
-nextBtn.addEventListener("click", () => {
-    page < 500 ? page++ : page;
-    return page;
-})
+// const getPopularMovies = getMovies(API_URL);
 
 async function getMovies (url) {
     const res = await fetch(url);
@@ -83,7 +74,9 @@ async function getMovies (url) {
     return data.results;
 } 
 
-const getPopularMovies = getMovies(API_URL + page).then(movies => {
+getMovies(API_URL + page).then(movies => {
+
+    prevBtn.setAttribute("disabled", "")
 
     movies.forEach(movie => {
         const { title, vote_average, overview, poster_path} = movie;
@@ -102,11 +95,93 @@ const getPopularMovies = getMovies(API_URL + page).then(movies => {
                 <h3>Overview</h3>
                 ${overview}
             </div>
-        </div>`
-
+        </div>
+        `
         container.appendChild(movieEl)
     });
 })
+
+//Fetch and display the next array of popular movies when client click next and update buttons
+
+nextBtn.addEventListener("click", () => {
+
+    if (page === maxPage - 1) {
+        nextBtn.setAttribute("disabled", "");
+        page++;
+    } else if (page < maxPage - 1) {
+        page++;
+        nextBtn.previousElementSibling.disabled = false;
+    } 
+
+    container.innerHTML = ""
+
+    getMovies(API_URL + page).then(movies => {
+
+        movies.forEach(movie => {
+            const { title, vote_average, overview, poster_path} = movie;
+    
+            let movieEl = document.createElement("div");
+            movieEl.classList.add("movie");
+    
+            movieEl.innerHTML = `
+            <div class="movie">
+                <img src=${IMG_PATH}${poster_path} alt="">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${noteColor(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+            </div>`
+    
+            container.appendChild(movieEl)
+        });
+    })
+})
+
+//Fetch and display the previous array of popular movies when client click previous
+
+prevBtn.addEventListener("click", () => {
+
+    if (page === 2) {
+        prevBtn.setAttribute("disabled", "");
+        page--;
+    } else if (page > 2) {
+        prevBtn.nextElementSibling.disabled = false;
+        page--;
+    }
+
+    container.innerHTML = ""
+    
+    getMovies(API_URL + page).then(movies => {
+
+        movies.forEach(movie => {
+            const { title, vote_average, overview, poster_path} = movie;
+    
+            let movieEl = document.createElement("div");
+            movieEl.classList.add("movie");
+    
+            movieEl.innerHTML = `
+            <div class="movie">
+                <img src=${IMG_PATH}${poster_path} alt="">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${noteColor(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+            </div>`
+    
+            container.appendChild(movieEl)
+        });
+    })
+})
+
+
 
 function noteColor(vote) {
     if (vote < 5) {
@@ -117,3 +192,14 @@ function noteColor(vote) {
         return "green";
     }            
 }
+
+function colors () {
+    const rootEl = document.querySelector(':root');
+    rootEl.classList.toggle("active")
+    // console.log(rootEl)
+}
+
+colorSwitch.addEventListener("click", () => {
+    colors()
+})
+
