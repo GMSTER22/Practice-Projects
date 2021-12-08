dc0902bbf5d2e81c1f738fa10daa14a008bf4610
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-12-08 13:08:38 -0500

    complete javascript logic of the app

--- 18-Background slider/script.js
+++ 18-Background slider/script.js
@@ -1,8 +1,30 @@
 const leftArrow = document.querySelector(".slider_box-arrow.left");
 const rightArrow = document.querySelector(".slider_box-arrow.right");
-const images = document.querySelectorAll(".slider_box-img > img");
-console.log(images)
+const slides = document.querySelectorAll(".slide");
+const container = document.querySelector(".container");
 
-images.addEventListener("click", () => {
-    alert("bou")
-})
+let activeSlide = 0;
+
+rightArrow.addEventListener("click", () => {
+    activeSlide < slides.length -1 ? activeSlide++ : activeSlide = 0;
+    setActiveSlide();
+    setBgToContainer();
+});
+
+leftArrow.addEventListener("click", () => {
+    activeSlide <= 0 ? activeSlide = slides.length - 1 : activeSlide--;
+    setActiveSlide();
+    setBgToContainer();
+});
+
+function setBgToContainer() {
+    container.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    })
+
+    slides[activeSlide].classList.add("active");
+}
