f5d1f687309ae70918a3d35d325eb713dc6aaffb
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-12-07 23:43:26 -0500

    initialize background slider app and css styling

---
+++ 18-Background slider/script.js
@@ -0,0 +1,8 @@
+const leftArrow = document.querySelector(".slider_box-arrow.left");
+const rightArrow = document.querySelector(".slider_box-arrow.right");
+const images = document.querySelectorAll(".slider_box-img > img");
+console.log(images)
+
+images.addEventListener("click", () => {
+    alert("bou")
+})
