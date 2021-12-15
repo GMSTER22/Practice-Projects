af5a76b7a810b6c0788c1636d9d96fbf8359d0fa
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-12-14 19:38:05 -0500

    complete Image carousel project

--- 35-Image carousel/script.js
+++ 35-Image carousel/script.js
@@ -1,20 +1,41 @@
 const images = document.getElementById("imgs");
 const previousBtn = document.getElementById("left");
 const nextBtn = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
 const numberOfImages = images.children.length;
+
 let times = 0
 
-function slidding() {
-    setInterval(()=> {
-        images.style.transform = `translateX(${times * -50}rem)`;
+let interval = setInterval(()=> run, 2000);
 
-        times < numberOfImages - 1 ? times++ : times = 0 
-        
-    },1000)
+function run() {
+    times++
+    changeImage()
+}
+
+function changeImage() {
+    if (times > numberOfImages - 1) {
+        times = 0
+    } else if (times < 0) {
+        times = numberOfImages - 1
+    }
+    images.style.transform = `translateX(${times * -50}rem)`;  
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
 }
 
 previousBtn.addEventListener("click", ()=> {
-    times <= numberOfImages - 1 ? times-- : times = 0;
+    times--
+    changeImage()
+    resetInterval()
 })
 
-slidding()
+nextBtn.addEventListener("click", ()=> {
+    times++
+    changeImage()
+    resetInterval()
+})
