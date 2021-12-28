2fd66ed0cdcb7a9bdddad6a36d7cbc7c383ce06b
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-12-28 11:34:54 -0500

    add messages to display

--- 27--Toast notification/script.js
+++ 27--Toast notification/script.js
@@ -2,6 +2,7 @@ const notificationBtn = document.querySelector(".show-notification-btn");
 const notificationList = document.querySelector(".notification-list");
 
 const notificationStatus = ["info", "success", "failure"];
+const message = ["Message One", "Message Two", "Message Three", "Message Four"];
 
 notificationBtn.addEventListener("click", () => {
     let random = (element) =>  Math.floor(Math.random()* element.length);
@@ -9,11 +10,11 @@ notificationBtn.addEventListener("click", () => {
     let li = document.createElement("li");
     let btn = document.createElement("button");
     li.setAttribute("class", `notification-item ${notificationStatus[random(notificationStatus)]}`);
-    btn.textContent = "Message";
+    btn.textContent = `${message[random(message)]}`;
     li.appendChild(btn);
     notificationList.appendChild(li);
 
-    setTimeout( ()=> li.remove() ,2500)
+    setTimeout( ()=> li.remove() ,3000)
 })
 
 
