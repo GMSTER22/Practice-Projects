fe3a9bfce4e527a22f6f2977115625a75953540a
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-01-03 21:47:19 -0500

    finish add and delete interactions to the DOM

--- 33- Notes App/script.js
+++ 33- Notes App/script.js
@@ -1,18 +1,44 @@
-const container = document.querySelector(".container");
+const container = document.querySelector("body");
 const addBtn = document.getElementById("add");
-const writeBtn = document.getElementById("write");
-const deleteBtn = document.getElementById("delete");
+
 const noteCard = document.getElementById("note");
 
-addBtn.addEventListener("click", ()=> {
-   const newNote = `
-                        <div class="note" id="note">
+
+addBtn.addEventListener("click", () => addNewNote("Hello world"));
+
+function addNewNote (text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
                         <div class="note__options">
                             <button id="write"><i class="fa-solid fa-pen-to-square"></i></button>
                             <button id="delete"><i class="fa-solid fa-trash-can"></i></button>
                         </div>
-                        <textarea name="note__textarea" id="note__textarea" autofocus></textarea>
-                        </div>` 
 
-    container.appendChild(newNote);
-});
+                        <div class="main ${ text? "" : "hidden" }"></div>
+                        <textarea name="note__textarea" id="textarea" class="${ text? "hidden" : "" }" ></textarea>`;
+
+    container.appendChild(note);
+
+    const writeBtn = note.querySelector("#write");
+    const deleteBtn = note.querySelector("#delete");
+    const textareaEl = note.querySelector("#textarea");
+    const mainDiv = note.querySelector(".main");
+
+    // textareaEl.value = text;
+    // mainDiv.innerHTML = text;
+
+    writeBtn.addEventListener("click", ()=> {
+        textareaEl.classList.toggle("hidden");
+        mainDiv.classList.toggle("hidden");
+    });
+
+    deleteBtn.addEventListener("click", ()=> {
+        note.remove();
+    });
+
+    textareaEl.addEventListener("change", (e)=> {
+        const { value } = e.target;
+        mainDiv.innerHTML = value;
+    });
+};
