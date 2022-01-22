95c08de23b052b1971a6bcf500accc640ec6cdf3
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-01-22 17:50:12 -0500

    update custom range slider project

--- 44- Custom Range Slider/script.js
+++ 44- Custom Range Slider/script.js
@@ -1,15 +1,33 @@
 const track = document.getElementById("range");
 
+let thumbStartingPosition = getComputedStyle(track.nextElementSibling).
+    getPropertyValue("left");
+    thumbStartingPosition = +thumbStartingPosition.substring(0, thumbStartingPosition.length - 2);
+    console.log(thumbStartingPosition)
+
 
 track.addEventListener("input", (e) => {
+
+    //get values of track and thumb width
+    let trackWidth = getComputedStyle(e.target).getPropertyValue("width");    
+    let thumbWidth = getComputedStyle(e.target.nextElementSibling).
+    getPropertyValue("width");
+
+    //transform values into numbers
+    trackWidth = +trackWidth.substring(0, trackWidth.length - 2);
+    thumbWidth = +thumbWidth.substring(0, thumbWidth.length - 2);
+
+    //get the distance the thumb must cover on the left and right
+    const leftDistance = thumbStartingPosition - (trackWidth / 2) + 10;
+    const rightDistance = thumbStartingPosition + (trackWidth / 2) - 10;
+
+    // console.log( e.target.nextElementSibling.style.left);
+
     const min = +e.target.min;
     const max = +e.target.max;
 
-    console.log(e.target)
-    console.log(e, min, max)
     e.target.nextElementSibling.innerText = e.target.value;
-    e.target.nextElementSibling.style.left = `${scale(e.target.value, min, max, -30, 245)}px`;
-
+    e.target.nextElementSibling.style.left = `${scale(e.target.value, min, max, leftDistance, rightDistance)}px`;
 });
 
 //function used to convert a range of number to another range of number
