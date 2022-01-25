73ca624633dcaee59011204de8cc4f1f0b7d74da
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-01-25 18:20:45 -0500

    complete Netflix Navigation

--- 45 - Netflix Navigation/script.js
+++ 45 - Netflix Navigation/script.js
@@ -1,5 +1,15 @@
-const nav = document.getElementById("nav");
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
 
-nav.addEventListener("animationend", (e) => {
-    
+openBtn.addEventListener("click", () => {
+    nav.forEach(navEl => {
+        navEl.classList.add("visible");
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    nav.forEach(navEl => {
+        navEl.classList.remove("visible");
+    });
 });
